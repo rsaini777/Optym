f3662547d922729ce32b940f7b8cf2a612177051
rsaini777 <46646514+rsaini777@users.noreply.github.com>
2022-05-15 16:00:49 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,74 +1,137 @@
 import "./App.css";
 import React, { useState } from "react";
-import Image from "./Image"
-import { saveAs } from 'file-saver';
-
-
+import Image from "./Image";
 
 function App() {
-  const[results,setResults]=useState([])
-  const[value,setValue]=useState("")
-  const shuffleData=["fashion","film","car","bike","boat","nature","sunset","dark","food","ocean","Textures & Patterns","beauty","lake","forest","moon","action","animal","lion","kids","gods","history"]
-  const [random,setRandom]=useState(shuffleData[0])
+ 
+  const [results, setResults] = useState([]);
+  const [value, setValue] = useState("");
+  const shuffleData = [
+    "fashion",
+    "film",
+    "car",
+    "bike",
+    "boat",
+    "nature",
+    "sunset",
+    "dark",
+    "food",
+    "ocean",
+    "Textures & Patterns",
+    "beauty",
+    "lake",
+    "forest",
+    "moon",
+    "action",
+    "animal",
+    "lion",
+    "kids",
+    "gods",
+    "history",
+  ];
+  const [random, setRandom] = useState(shuffleData[0]);
   const [pageNumber, setPageNumber] = useState(0);
-  const [pageRange,setPageRange]=useState(2)
+  const [pageRange, setPageRange] = useState(2);
 
- 
-  const next=(e)=>{
-    if(pageRange<=20){
-    setPageNumber(pageNumber+2)
-    setPageRange(pageRange+2)
-  }
-    e.preventDefault()
-  }
-  const previous=(e)=>{
-    if(pageNumber>=0){
-    setPageNumber(pageNumber-2)
-    setPageRange(pageRange-2)
-  }
-     e.preventDefault()
-  }
-  const search=(e)=>{
-    setValue("")
-    fetch(`https://api.unsplash.com/search/photos/?client_id=Bn1bLki8GqBZxK6bCE9MpLlKUvlVeB34M1Trm7A2was&per_page=20&query=${value}`)
-    .then(res=>res.json())
-     .then(data=>{
-       console.log(data)
-       setResults(data.results)
-     })
-     e.preventDefault()
-  }
-  
+  const next = (e) => {
+    if (pageRange <= 20) {
+      setPageNumber(pageNumber + 2);
+      setPageRange(pageRange + 2);
+    }
+    e.preventDefault();
+  };
+  const previous = (e) => {
+    if (pageNumber >= 0) {
+      setPageNumber(pageNumber - 2);
+      setPageRange(pageRange - 2);
+    }
+    e.preventDefault();
+  };
+  const search = (e) => {
+    setValue("");
+    fetch(
+      `https://api.unsplash.com/search/photos/?client_id=Bn1bLki8GqBZxK6bCE9MpLlKUvlVeB34M1Trm7A2was&per_page=20&query=${value}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setResults(data.results);
+      });
+    e.preventDefault();
+  };
 
-   const shuffle=()=>{
-    let differentValue=Math.floor(Math.random() *20)
-    setRandom(shuffleData[differentValue])
-   }
+  const shuffle = () => {
+    let differentValue = Math.floor(Math.random() * 20);
+    setRandom(shuffleData[differentValue]);
+  };
 
-  const fetchData=()=>{
-    shuffle()
-    fetch(`https://api.unsplash.com/search/photos/?client_id=Bn1bLki8GqBZxK6bCE9MpLlKUvlVeB34M1Trm7A2was&per_page=20&query=${random}`)
-     .then(res=>res.json())
-      .then(data=>{
-        setResults(data.results)
-      })
-  }
-  const download = () => {
-    const data="https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb";
-    saveAs(data)
-  }
- 
+  const fetchData = () => {
+    shuffle();
+    fetch(
+      `https://api.unsplash.com/search/photos/?client_id=Bn1bLki8GqBZxK6bCE9MpLlKUvlVeB34M1Trm7A2was&per_page=20&query=${random}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setResults(data.results);
+      });
+  };
+
   return (
     <div className="App">
-      <input value={value} onChange={(e)=>setValue(e.target.value)}></input>
-      <button onClick={()=>search()}>Search</button>
-      <button onClick={()=>fetchData()}>Shuffle</button>
-      <Image results={results} pageNumber={pageNumber} pageRange={pageRange}></Image>
-     
-      <button onClick={()=>previous()}>previous</button>
-      <button onClick={()=>next()}>next</button>
-      <button onClick={()=>download()}>Download</button>
+      <div className="d-flex justify-content-center text-center mt-5 justify-content-around">
+        <h1 className="logo">Gallery</h1>
+        <div>
+          <div className="search_wrap search_wrap_3">
+            <div className="search_box">
+              <input
+                type="text"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                className="input"
+                placeholder="search..."
+              />
+              <div className="btn btn_common">
+                <button
+                  type="button"
+                  className="downlaod-btn btn"
+                  onClick={() => search()}
+                >
+                  <i className=" fas ri-search-line"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="text-center">
+          <button onClick={() => fetchData()} className=" btn-dark shuffle">
+            Shuffle <i className="ri-shuffle-fill"></i>
+          </button>
+        </div>
+      </div>
+      {/* first row */}
       
+        <Image
+          results={results}
+          pageNumber={pageNumber}
+          pageRange={pageRange}
+          className="display"
+        ></Image>
+  
+
+      <div className="d-flex mt-5 justify-content-center ">
+        <button
+          onClick={() => previous()}
+          className="ms-5 previous px-4 py-2 display-6"
+        >
+          <i className="ri-arrow-left-s-line"></i>
+        </button>
+        <button
+          onClick={() => next()}
+          className="ms-5 next px-4 py-2 display-6"
+        >
+          <i className="ri-arrow-right-s-line"></i>
+        </button>
+      </div>
     </div>
   );
 }
--- src/Image.js
+++ src/Image.js
@@ -1,19 +1,52 @@
-import React from 'react'
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
+import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import { saveAs } from "file-saver";
 
-function Image({results,pageNumber,pageRange}) {
+function Image({ results, pageNumber, pageRange }) {
+  const download = (ImageResult) => {
+    saveAs(ImageResult);
+  };
+  console.log(results)
   return (
     <div>
-         {
-        results
-         .slice(pageNumber, pageRange)
-         .map((item)=>(
-          <LazyLoadImage effect="blur" alt="" key={item.id} src={item.urls.small}></LazyLoadImage>
-        ))
-      }
+      {results.slice(pageNumber, pageRange).map((item) => (
+        <div className=" d-flex">
+          <div className="">
+            <div className="card mt-5 cards mx-auto">
+              <div className="card-body">
+                <LazyLoadImage
+                  effect="blur"
+                  alt=""
+                  key={item.id}
+                  src={item.urls.regular}
+                  className="images"
+                ></LazyLoadImage>
+                <div className="d-flex justify-content-between ">
+                  <div >
+                    <img alt="" className="avatar" src={item.user.profile_image.medium}></img>
+                  </div>
+                  <div className="text-start ">
+                    <h5 className="personal-info ">{item.user.name}</h5>
+                    <p className="likes ">{item.likes} Likes recieved</p>
+                  </div>
+                  <div className="download">
+                  
+                    <button
+                      className="downlaod-btn btn  "
+                      onClick={() => download(`${item.links.download}`)}
+                    >
+                      <i class="ri-download-2-line display-6"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Image
+export default Image;
